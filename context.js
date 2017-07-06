sywac/sywac

commit 0081c19f6c16fff17d2deb15f09d1749808aedee

feat: support concurrent parsing (#1)
functional programming and the evil proliferation of context

move value state storage out of types and into context

now api supports concurrent parse executions

death to type reset

context.js
@@ -16,6 +16,9 @@ class Context {
     // args to parse per type
     this.args = []
     this.slurped = []
+    // values by type, keyed by type.id
+    this.values = new Map()
+    this.sources = new Map()
     // results of parsing and validation
     this.code = 0
     this.output = ''
@@ -203,6 +206,35 @@ class Context {
     if (typeof this.versionRequested.version === 'function') this.output = this.versionRequested.version()
     else this.output = this.versionRequested.version
     return this
+  }
+
+  // weird method names make for easier code searching
+  assignValue (id, value) {
+    this.values.set(id, value)
+  }
+
+  lookupValue (id) {
+    return this.values.get(id)
+  }
+
+  employSource (id, source, position, raw) {
+    let obj = this.lookupSource(id)
+    if (!obj) {
+      obj = { source: undefined, position: [], raw: [] }
+      this.sources.set(id, obj)
+    }
+    if (typeof source === 'string') obj.source = source
+    if (typeof position === 'number') obj.position.push(position)
+    if (typeof raw === 'string') obj.raw.push(raw)
+  }
+
+  lookupSource (id) {
+    return this.sources.get(id)
+  }
+
+  lookupSourceValue (id) {
+    const obj = this.lookupSource(id)
+    return obj && obj.source
   }
 
   populateArgv (typeResults) {

types/unknown.js
@@ -78,24 +78,20 @@ class TypeUnknown extends Type {
       if (implicitCommands.length) unparsed = this._tryImplicitCommand(unparsed, context, implicitCommands)
     }
 
-    // console.log(`unparsed before positionals`, unparsed)
     if (unparsed.length && this.positionals && this.positionals.length) {
-      unparsed = this._populatePositionals(unparsed)
-      // this.positionals.forEach(p => p.validateParsed(context))
+      unparsed = this._populatePositionals(unparsed, context)
     }
-    // console.log(`unparsed after positionals`, unparsed)
 
-    this._value = unparsed.map(arg => {
-      this._addPosition(arg.index)
-      this._addRaw(arg.raw)
+    const v = unparsed.map(arg => {
+      this.applySource(context, null, arg.index, arg.raw)
       return arg.raw
     }).concat(context.details.args.slice(context.args.length).map((arg, index) => {
-      this._addPosition(context.args.length + index)
-      this._addRaw(arg)
+      this.applySource(context, null, context.args.length + index, arg)
       return arg
     }))
+    context.assignValue(this.id, v)
 
-    if (this._value.length > 0) this._source = Type.SOURCE_POSITIONAL
+    if (v.length > 0) this.applySource(context, Type.SOURCE_POSITIONAL)
 
     if (this.positionals && this.positionals.length) {
       return Promise.all(this.positionals.map(p => p.validateParsed(context))).then(whenDone => super.resolve())
@@ -109,7 +105,7 @@ class TypeUnknown extends Type {
     let matched = implicitCommands.find(alias => alias === first.raw) // maybe indexOf would be better/faster?
     if (matched) {
       context.slurped[first.index].parsed[0].claimed = true
-      this.implicit[matched].implicitCommandFound(Type.SOURCE_POSITIONAL, first.index, first.raw, context)
+      this.implicit[matched].implicitCommandFound(context, Type.SOURCE_POSITIONAL, first.index, first.raw)
       return unparsed.slice(1)
     }
     return unparsed
@@ -133,7 +129,7 @@ class TypeUnknown extends Type {
   // b = two
   // c = three four
 
-  _populatePositionals (unparsed) {
+  _populatePositionals (unparsed, context) {
     // filter out positionals already populated via flags
     // (can populate via flags or positional args, but not both at same time)
     let positionals = this.positionals.filter(p => p.source !== Type.SOURCE_FLAG)
@@ -153,8 +149,8 @@ class TypeUnknown extends Type {
       }
 
       // assign value and decrement numArgsLeft
-      current.setValue(arg.raw)
-      current.applySource(Type.SOURCE_POSITIONAL, arg.index, arg.raw)
+      current.setValue(context, arg.raw)
+      current.applySource(context, Type.SOURCE_POSITIONAL, arg.index, arg.raw)
       numArgsLeft--
 
       // determine if we should move on to the next positional

api.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const SOURCE_DEFAULT = require('./types/type').SOURCE_DEFAULT
+
 class Api {
   static get DEFAULT_COMMAND_INDICATOR () {
     return '*'
@@ -505,7 +507,15 @@ class Api {
   parse (args) {
     // init context and kick off recursive type parsing/execution
     let context = this.initContext(false).slurpArgs(args)
+
+    // init unknownType in context only for the top-level (all levels share/overwrite the same argv._)
+    if (this.unknownType) {
+      this.unknownType.setValue(context, this.unknownType.defaultVal)
+      this.unknownType.applySource(context, SOURCE_DEFAULT)
+    }
+
     if (this._showHelpByDefault && !context.details.args.length) context.deferHelp() // preemptively request help
+
     return this.parseFromContext(context).then(whenDone => {
       if (context.helpRequested && !context.output) {
         context.addDeferredHelp(this.initHelpBuffer())
@@ -518,12 +528,6 @@ class Api {
     }).catch(err => {
       context.unexpectedError(err)
     }).then(whenDone => {
-      try {
-        this.types.forEach(type => type.reset())
-        if (this.unknownType) this.unknownType.reset()
-      } catch (err) {
-        context.unexpectedError(err)
-      }
       return context.toResult()
     })
   }
@@ -561,11 +565,11 @@ class Api {
     }).then(whenDone => {
       // once all parsing is complete, populate argv in context (sync)
       // first add unknownType to context.argv (because it's needed to determine shouldCoerceAndCheck)
-      if (this.unknownType) context.populateArgv([this.unknownType.toResult(true)])
+      if (this.unknownType) context.populateArgv([this.unknownType.toResult(context, true)])
       // next determine shouldCoerceAndCheck
       const shouldCoerceAndCheck = this.shouldCoerceAndCheck(context, hasCommands, hasDefaultCommand)
       // then populate argv with other types, letting them know if it makes sense to apply coercion
-      context.populateArgv(this.types.map(type => type.toResult(shouldCoerceAndCheck)))
+      context.populateArgv(this.types.map(type => type.toResult(context, shouldCoerceAndCheck)))
 
       // TODO before postParse, determine if any are promptable (and need prompting) and prompt each in series
 
@@ -587,7 +591,11 @@ class Api {
   }
 
   applyTypes (context) {
-    context.pushLevel(this.name, this.types.map(type => type.toObject()))
+    context.pushLevel(this.name, this.types.map(type => {
+      type.setValue(context, type.defaultVal)
+      type.applySource(context, SOURCE_DEFAULT)
+      return type.toObject()
+    }))
     return context
   }
 

lib/utils.js
@@ -146,7 +146,7 @@ class Utils {
       firstEqual = value.indexOf('=')
       if (firstEqual !== -1) {
         props.defaultValue = value.slice(firstEqual + 1)
-        if (props.variadic) props.defaultValue = [props.defaultValue] // TODO TypeArray with defaultValue has problems!
+        if (props.variadic) props.defaultValue = [props.defaultValue]
         value = value.slice(0, firstEqual)
       }
 

test/test-parse.js
@@ -154,7 +154,7 @@ tap.test('api parse > multiple sequential passes', t => {
   })
 })
 
-tap.test('api parse > multiple concurrent passes', {skip: true}, t => {
+tap.test('api parse > multiple concurrent passes', t => {
   const api = Api.get()
     .boolean('--bool | -b')
     .string('--str  | -s <value>')

types/array.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const SOURCE_DEFAULT = require('./type').SOURCE_DEFAULT
 const TypeWrapper = require('./wrapper')
 
 class TypeArray extends TypeWrapper {
@@ -8,7 +9,7 @@ class TypeArray extends TypeWrapper {
   }
 
   constructor (opts) {
-    super(Object.assign({ defaultValue: [], delim: ',', cumulative: true }, opts || {}))
+    super(Object.assign({ delim: ',', cumulative: true }, opts || {}))
   }
 
   configure (opts, override) {
@@ -40,6 +41,11 @@ class TypeArray extends TypeWrapper {
     return this
   }
 
+  get defaultVal () {
+    const dv = super.defaultVal
+    return dv ? [].concat(dv) : [] // DO NOT LET getValue AND defaultVal REFERENCE THE SAME ARRAY OBJECT!
+  }
+
   get datatype () {
     let subtype = this.elementType.datatype
     return 'array' + (subtype ? `:${subtype}` : '')
@@ -51,43 +57,49 @@ class TypeArray extends TypeWrapper {
     if (datatypeIndex !== -1) hints[datatypeIndex] = this.datatype
   }
 
-  isApplicable (currentValue, previousValue, slurpedArg) {
+  isApplicable (context, currentValue, previousValue, slurpedArg) {
     // remove last element if previous value was not explicit
-    if (this._value && this._value.length && typeof previousValue !== 'string') {
-      this._value = this._value.slice(0, -1)
-    }
-    this.elementType.isApplicable(currentValue, previousValue, slurpedArg)
+    let v = context.lookupValue(this.id)
+    if (v && v.length && typeof previousValue !== 'string') v.pop()
+    this.elementType.isApplicable(context, currentValue, previousValue, slurpedArg)
     return true // TODO this is greedy (`--key=one two` includes `one` and `two`), make this configurable
   }
 
-  observeAlias (alias) {
-    if (!this._cumulative) this._value = []
-    this.elementType.observeAlias(alias)
+  observeAlias (context, alias) {
+    if (!this._cumulative) context.assignValue(this.id, [])
+    this.elementType.observeAlias(context, alias)
   }
 
-  setValue (value) {
-    // console.log('array.js > setValue:', value, 'for', this.helpFlags)
+  setValue (context, value) {
+    // if current source is 'default', then we're now setting a non-default value
+    // so append to a fresh array instead of the default one
+    // note that this assumes setValue is called before applySource in api.applyTypes
+    if (context.lookupSourceValue(this.id) === SOURCE_DEFAULT) context.assignValue(this.id, [])
+
     if (Array.isArray(value)) {
-      this._value = (this._value || []).concat(value)
+      let v = context.lookupValue(this.id)
+      context.assignValue(this.id, (v || []).concat(value))
       return
     }
     if (value && this.delim && typeof value === 'string') {
-      value.split(this.delim).forEach(v => this.addValue(v))
+      value.split(this.delim).forEach(v => this.addValue(context, v))
       return
     }
-    this.addValue(value)
+    this.addValue(context, value)
   }
 
-  addValue (value) {
-    this.elementType.setValue(value)
-    if (!this._value) this._value = []
-    // if (!this._value || !Array.isArray(this._value)) this._value = []
-    let elementValue = this.elementType.value
-    if (Array.isArray(elementValue) && this._value.length && this._value[this._value.length - 1] === elementValue) {
+  addValue (context, value) {
+    this.elementType.setValue(context, value)
+    let v = context.lookupValue(this.id)
+    if (!v) {
+      v = []
+      context.assignValue(this.id, v)
+    }
+    let elementValue = this.elementType.getValue(context)
+    if (Array.isArray(elementValue) && v.length && v[v.length - 1] === elementValue) {
       return // we already have elementValue, it's just been modified
     }
-    // console.log('array.js > this._value:', this._value)
-    this._value.push(elementValue)
+    v.push(elementValue)
   }
 
   get isStrict () {
@@ -100,17 +112,12 @@ class TypeArray extends TypeWrapper {
     })
   }
 
-  buildInvalidMessage (msgAndArgs) {
-    super.buildInvalidMessage(msgAndArgs)
+  buildInvalidMessage (context, msgAndArgs) {
+    super.buildInvalidMessage(context, msgAndArgs)
     const sub = {}
-    this.elementType.buildInvalidMessage(sub)
+    this.elementType.buildInvalidMessage(context, sub)
     if (sub.msg) msgAndArgs.msg = sub.msg
     if (sub.args.length > msgAndArgs.args.length) msgAndArgs.args = msgAndArgs.args.concat(sub.args.slice(msgAndArgs.args.length))
-  }
-
-  reset () {
-    super.reset()
-    this._value = this._value.slice() // DO NOT LET _value AND _defaultVal REFERENCE THE SAME ARRAY OBJECT!
   }
 }
 

types/boolean.js
@@ -15,12 +15,12 @@ class TypeBoolean extends Type {
     return 'boolean'
   }
 
-  isApplicable (currentValue, previousValue, slurpedArg) {
+  isApplicable (context, currentValue, previousValue, slurpedArg) {
     return typeof currentValue === 'boolean' || currentValue === 'true' || currentValue === 'false'
   }
 
-  setValue (value) {
-    this._value = typeof value === 'boolean' ? value : value === 'true'
+  setValue (context, value) {
+    context.assignValue(this.id, typeof value === 'boolean' ? value : value === 'true')
   }
 }
 

types/command.js
@@ -8,6 +8,11 @@ class TypeCommand extends Type {
     return new TypeCommand(opts)
   }
 
+  constructor (opts) {
+    // default value is for benefit of context.details.types
+    super(Object.assign({ defaultValue: false }, opts))
+  }
+
   configure (opts, override) {
     opts = opts || {}
     if (typeof override === 'undefined') override = true
@@ -87,11 +92,11 @@ class TypeCommand extends Type {
       let matchedArg = context.slurped.find(arg => arg.parsed.length === 1 && !arg.parsed[0].key && !arg.parsed[0].claimed)
       if (matchedArg) {
         matchedArg.parsed[0].claimed = true
-        this.applySource(Type.SOURCE_POSITIONAL, matchedArg.index, matchedArg.raw)
+        this.applySource(context, Type.SOURCE_POSITIONAL, matchedArg.index, matchedArg.raw)
       }
     }
-    this.setValue(true) // set this value to true for context.details
-    context.populateArgv([this.toResult()]) // apply value to context.details
+    this.setValue(context, true) // set this value to true for context.details
+    context.populateArgv([this.toResult(context)]) // apply value to context.details
 
     // add positionals from preconfigured opts
     if (typeof this._positionalDsl === 'string' && this._positionalDsl.length) {

commit ef9cda1488dc0bb024e33141bc9fb481f77bc60b

fix unknown type source/position/raw when running commands
was regression introduced with 0081c19

context.js
@@ -217,6 +217,10 @@ class Context {
     return this.values.get(id)
   }
 
+  resetSource (id, source) {
+    this.sources.set(id, { source: source, position: [], raw: [] })
+  }
+
   employSource (id, source, position, raw) {
     let obj = this.lookupSource(id)
     if (!obj) {

types/unknown.js
@@ -82,6 +82,7 @@ class TypeUnknown extends Type {
       unparsed = this._populatePositionals(unparsed, context)
     }
 
+    context.resetSource(this.id, Type.SOURCE_DEFAULT)
     const v = unparsed.map(arg => {
       this.applySource(context, null, arg.index, arg.raw)
       return arg.raw